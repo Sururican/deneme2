9797c278d30c73451499c9daaf4f29b6ff5b8a6b
Sururican <86240386+Sururican@users.noreply.github.com>
2023-12-26 15:29:26 +0100

    Update LSTM_Prediction.py

--- main/LSTM_Prediction.py
+++ main/LSTM_Prediction.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 
 def make_predictions(input_data):
     # Load the saved model
-    model_folder='LSTM_Model'
+    model_folder='LSTM_Modell'
     loaded_model = tf.keras.models.load_model(model_folder)
 
     # Preprocess input_data as needed
